2516387f5283b9e6e15b8b2407b00e2f881b6230
Ritesh Patel <riteshzeronity@gmail.com>
2022-03-16 13:00:07 +0530

    Initial commit, setup the project with LinearLayout, TableLayout, ConstraintLayout, CardView, and ScrollView

---
+++ app/src/main/java/com/example/exploringlayouts/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.exploringlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu)
+    }
+
+    fun loadConstraintLayout(v: View) {
+        setContentView(R.layout.activity_main);
+    }
+
+    fun loadTableLayout(v: View) {
+        setContentView(R.layout.my_table_layout);
+    }
+
+    fun loadMainMenu(v: View) {
+        setContentView(R.layout.main_menu);
+    }
+
+    fun loadCardAndScroll(v: View) {
+        setContentView(R.layout.cardview_and_scrollview);
+    }
+}
